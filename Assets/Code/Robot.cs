f0158a70e4ace55911c479279c509c490d7913dc
Henri Viitanen <henri.viitanen@gmail.com>
2021-01-30 13:47:22 +0200

    robot struggle movement

--- Assets/Code/Robot.cs
+++ Assets/Code/Robot.cs
@@ -5,18 +5,26 @@ using TMPro;
 
 public class Robot : MonoBehaviour
 {
+    enum State
+    {
+        Unreeling,
+        Struggle
+    }
+
     [SerializeField] private GameObject _lineRendererPrefab;
     [SerializeField] private TextMeshProUGUI _hudText;
 
     [SerializeField] private float _lineSegmentLength = 1f;
-    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private float _minMoveSpeed = .2f;
+    [SerializeField] private float _maxMoveSpeed = 5f;
     [SerializeField] private int _maxSegmentCount = 10;
 
     private Rigidbody2D _rigidbody;
     private LineRenderer _lineRenderer;
     private Vector2 _previousLinePosition;
     private int _currentLineIndex = 0;
-    private bool _canMove = true;
+    private State _currentState;
+    private float _currentMoveSpeed;
 
     void Start()
     {
@@ -30,6 +38,8 @@ public class Robot : MonoBehaviour
         _lineRenderer.positionCount++;
         _currentLineIndex++;
         _previousLinePosition = transform.position;
+
+        _currentState = State.Unreeling;
     }
 
     void Update()
@@ -38,14 +48,17 @@ public class Robot : MonoBehaviour
 
         if (_lineRenderer.positionCount >= _maxSegmentCount)
         {
-            _canMove = false;
+            _currentState = State.Struggle;
         }
 
-        if (_canMove)
+        if (_currentState == State.Unreeling)
         {
             _lineRenderer.SetPosition(_currentLineIndex, transform.position);
 
             var dist = Vector2.Distance(transform.position, _previousLinePosition);
+
+            _currentMoveSpeed = Mathf.Lerp(_maxMoveSpeed, _minMoveSpeed, dist / _lineSegmentLength);
+
             if (dist > _lineSegmentLength)
             {
                 _currentLineIndex++;
@@ -58,7 +71,7 @@ public class Robot : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (_canMove)
+        if (_currentState == State.Unreeling)
         {
             Vector2 dir = Vector2.zero;
 
@@ -79,7 +92,7 @@ public class Robot : MonoBehaviour
                 dir += Vector2.right;
             }
 
-            _rigidbody.MovePosition(_rigidbody.position + dir.normalized * _moveSpeed * Time.fixedDeltaTime);
+            _rigidbody.MovePosition(_rigidbody.position + dir.normalized * _currentMoveSpeed * Time.fixedDeltaTime);
         }
     }
 }
