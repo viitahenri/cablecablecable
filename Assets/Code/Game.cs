78d5324b255863dd9ea23d90acc1a5b9eddcb1ab
Henri Viitanen <henri.viitanen@gmail.com>
2021-01-30 17:48:56 +0200

    Added map to Game scene (M toggles map)

--- Assets/Code/Game.cs
+++ Assets/Code/Game.cs
@@ -7,12 +7,34 @@ public class Game : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera _virtualCamera;
     [SerializeField] private GameObject _robotPrefab;
+    [SerializeField] private GameObject _map;
 
+    private Camera _mapCamera;
+    private bool _mapVisible = false;
     private GameObject _currentRobot;
 
     void Start()
     {
+        _mapCamera = _map.GetComponent<Camera>();
+        _map.SetActive(_mapVisible);
+        _mapCamera.enabled = _mapVisible;
+
         SpawnRobot();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ToggleMap();
+        }
+    }
+
+    void ToggleMap()
+    {
+        _mapVisible = !_mapVisible;
+        _map.SetActive(_mapVisible);
+        _mapCamera.enabled = _mapVisible;
     }
 
     void SpawnRobot()
