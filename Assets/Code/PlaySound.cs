622ef51aa7b07647c6c3aa0819fa8c154288f738
Henri Viitanen <henri.viitanen@gmail.com>
2021-01-31 11:45:31 +0200

    splash screen sounds

--- Assets/Code/PlaySound.cs
+++ Assets/Code/PlaySound.cs
@@ -19,4 +19,13 @@ public class PlaySound : MonoBehaviour
         if (_audioClips.Count > 0)
             _audioSource.PlayOneShot(_audioClips[Random.Range(0, _audioClips.Count)]);
     }
+
+    public void PlayByName(string name)
+    {
+        var found = _audioClips.Find(a => a.name == name);
+        if (found)
+        {
+            _audioSource.PlayOneShot(found);
+        }
+    }
 }
